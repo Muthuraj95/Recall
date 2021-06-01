0299ce8282dc2ac57b442cacbb0310de3e13ce27
Nithi.306 <muthunesik@gmail.com>
2021-06-01 15:38:46 -0700

    commit

---
+++ workspace/Newworkspace/src/mybranch/Fileupdation.java
@@ -0,0 +1,17 @@
+package mybranch;
+
+public class Fileupdation {
+public void muthuraj() {
+	// TODO Auto-generated method stub
+System.out.println("This is my name");
+}
+private void Rajeswari() {
+	// TODO Auto-generated method stub
+System.out.println("This is my mother's name");
+}
+public static void main(String[] args) {
+
+Fileupdation f = new Fileupdation();
+f.muthuraj();
+f.Rajeswari();
+}}
